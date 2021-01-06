12ea16f8a233a970706b5959cba53178409d78b5
Adriel Buenavides dos Santos <asantos@dataweb.com.br>
2021-01-06 17:08:57 -0300

    Protótipos: Adiciona mais protótipos do IdentityServer4 para estudo e prática

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -4,6 +4,8 @@
 
 using AutoMapper.Configuration;
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
 using System.Reflection;
 
 namespace IdentityServer
@@ -33,14 +36,13 @@ namespace IdentityServer
                  .AddInMemoryClients(Config.Clients)
                  .AddTestUsers(TestUsers.Users); */ //Usando informações em memória
 
-
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-4.0.0;trusted_connection=yes;";
 
             services.AddIdentityServer()
                 .AddTestUsers(TestUsers.Users)
-                .AddConfigurationStore(options => //adiciona as configurações do banco de dados 
-                {                                //Configurações do contexto do banco de dados e configurações de conexão e suporte com banco de dados 
+                .AddConfigurationStore(options =>
+                {
                     options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
                         sql => sql.MigrationsAssembly(migrationsAssembly));
                 })
@@ -71,6 +73,7 @@ namespace IdentityServer
 
             app.UseStaticFiles();
             app.UseRouting();
+            InitializeDatabase(app);
 
             app.UseIdentityServer();
             app.UseAuthorization();
@@ -79,6 +82,42 @@ namespace IdentityServer
             {
                 endpoints.MapDefaultControllerRoute();
             });
+        }
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
